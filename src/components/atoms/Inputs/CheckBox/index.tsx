c764e02ff32e710489f26cb4ee666eb34936056d
JwtnbRd <j-watanabe@ruby-dev.jp>
2025-01-20 04:36:58 +0900

    add CheckBox

---
+++ src/components/atoms/Inputs/CheckBox/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+type InputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & {
+  label?: string;
+  value?: string;
+  defaultChecked?: boolean;
+};
+
+export const CheckBox = React.forwardRef<HTMLInputElement, InputProps>(
+  function CheckBox(props, ref) {
+    const { label, value, defaultChecked, ...rest } = props;
+    return (
+      <>
+        <div>
+          <input
+            id={value ? `${rest.name}_${value}` : rest.name}
+            type="checkbox"
+            value={value}
+            defaultChecked={defaultChecked}
+            {...rest}
+            ref={ref}
+          />
+          <label htmlFor={value ? `${rest.name}_${value}` : rest.name}>
+            {label}
+          </label>
+        </div>
+      </>
+    );
+  }
+);
